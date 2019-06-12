9bd6f672e9f8f31b15cc0e340ccf3d2237c27fd7
shadow1687 <shadow1687@hotmail.com>
2019-06-11 22:06:41 -0300

    [fix] al editar y luego crear

--- js/clientes.js
+++ js/clientes.js
@@ -1,13 +1,12 @@
-
 var clientes = [];
 
-$(document).ready(function () {
+$(document).ready(function() {
     bindEvents();
     loadClientes();
 });
 
 function bindEvents() {
-    $(".send").off().click(function (e) {
+    $(".send").off().click(function(e) {
         e.preventDefault();
         e.stopPropagation();
         sendForm('.form-cliente');
@@ -21,7 +20,7 @@ function loadClientes() {
             "url": base_url() + "/sistemacontable/clientes_controller/load_cliente",
             "type": "post",
             "data": { 'st': 0 },
-            "dataSrc": function (json) {
+            "dataSrc": function(json) {
                 json.result.forEach(e => {
                     clientes[e.id] = e;
                 });
@@ -37,23 +36,27 @@ function loadClientes() {
         "sPaginationType": "numbers",
         "iDisplayLength": 100,
         "autoWidth": false,
-        "aLengthMenu": [[50, 100, 200, -1], [50, 100, 200, "Todos"]],
+        "aLengthMenu": [
+            [50, 100, 200, -1],
+            [50, 100, 200, "Todos"]
+        ],
         "stripeClasses": ["odd", "even"],
-        "columns": [
-            {
+        "columns": [{
                 title: "Cliente",
                 data: "razon_social",
                 sortable: true,
             },
             {
                 title: "Categoria IVA",
-                render: function (d, t, f) {
+                render: function(d, t, f) {
                     return _CATEGORIA_IVA[f.categoria_iva];
                 }
             },
             {
-                title: "Documento", data: "val_select", sortable: true,
-                render: function (d, t, f) {
+                title: "Documento",
+                data: "val_select",
+                sortable: true,
+                render: function(d, t, f) {
                     return _TIPO_DOCUMENTO[f.tipo_documento] + ' - ' + f.cuit;;
                 }
             },
@@ -65,7 +68,7 @@ function loadClientes() {
             },
             {
                 title: "Domicilio",
-                render: function (d, t, f) {
+                render: function(d, t, f) {
                     return `${f.domicilio} ${(f.altura) ? f.altura : ''} ${(f.piso) ? f.piso : ''} ${(f.depto) ? f.depto : ''}`;
                 }
             },
@@ -73,7 +76,7 @@ function loadClientes() {
             { title: "telefono", data: "telefono" },
             {
                 title: "",
-                render: function (d, t, f) {
+                render: function(d, t, f) {
                     let action_icons = ``;
                     action_icons += `<i class='fa fa-pen edit-cliente' title='Editar cliente'></i>&nbsp;`;
                     action_icons += `<i class='fa fa-times delete-cliente' title='Eliminar cliente'></i>`;
@@ -81,22 +84,24 @@ function loadClientes() {
                 }
             },
         ],
-        "createdRow": function (row, data, dataIndex) {
+        "createdRow": function(row, data, dataIndex) {
             $(row).attr('id', data.id);
         },
-        "order": [[0, 'desc'], [1, 'asc']],
-        "drawCallback": function () {
-            $('.delete-cliente').off().click(function () {
+        "order": [
+            [0, 'desc'],
+            [1, 'asc']
+        ],
+        "drawCallback": function() {
+            $('.delete-cliente').off().click(function() {
                 let id = $(this).closest('tr').attr('id');
                 eliminarCliente(id);
             });
-            $('.edit-cliente').off().click(function () {
+            $('.edit-cliente').off().click(function() {
                 let id = $(this).closest('tr').attr('id');
                 fillFormCliente(id);
             });
         },
-        "initComplete": function () {
-        }
+        "initComplete": function() {}
     };
 
     $("#tbl_cliente").DataTable(data_table_config);
@@ -109,21 +114,20 @@ function sendForm(id) {
         type: 'POST',
         url: base_url() + "/sistemacontable/clientes_controller/crear_cliente",
         data: data,
-        success: function (data, textStatus, request) {
+        success: function(data, textStatus, request) {
             if (data.success == false) {
                 if (data.result == null) {
                     setMessage('danger', data.msg, 4000);
                 }
                 //seteo los errores en el formlario
                 set_form_errors(id, data.response);
-            }
-            else {
+            } else {
+                $(id).attr('data-clientid', '');
                 clear_form(id);
                 $("#tbl_cliente").DataTable().ajax.reload();
             }
         },
-        error: function (request, textStatus, error) {
-        },
+        error: function(request, textStatus, error) {},
         dataType: 'json'
     });
 }
@@ -133,17 +137,15 @@ function eliminarCliente(client_id) {
         type: 'POST',
         url: base_url() + "/sistemacontable/clientes_controller/delete_cliente",
         data: { 'id': client_id },
-        success: function (data, textStatus, request) {
+        success: function(data, textStatus, request) {
             if (data.success == false) {
                 //seteo los errores en el formlario
                 set_form_errors(id, data.response);
-            }
-            else {
+            } else {
                 $("#tbl_cliente").DataTable().ajax.reload();
             }
         },
-        error: function (request, textStatus, error) {
-        },
+        error: function(request, textStatus, error) {},
         dataType: 'json'
     });
 }
